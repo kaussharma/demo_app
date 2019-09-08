8052d8fca575dfb4514cc5600a5d92d1f61a0111
w8l9in3 <extern.hari.parkash@volkswagen.de>
2019-09-08 15:27:45 +0200

    add initially ansible

--- terraform/security.tf
+++ terraform/security.tf
@@ -46,7 +46,7 @@ resource "aws_security_group" "secgroup_db" {
     to_port = 5432
     protocol = "tcp"
     security_groups = [aws_security_group.secgroup_instance.id]
-    description = " Allow public internet facing traffic on port 443"
+    description = " Allow DB access from ec2 instance"
   }
 
   egress {
@@ -58,103 +58,3 @@ resource "aws_security_group" "secgroup_db" {
   }
 
 }
-
-# resource "aws_security_group" "secgroup_esearch_kibana" {
-#   name = "sg.${var.org_scope}.${var.functional_scope}.${var.module_name}.esearch_kibana.${var.stage}"
-#   vpc_id = "${var.vpc_id}"
-
-#   ingress {
-#     from_port = 80
-#     to_port = 80
-#     protocol = "tcp"
-#     cidr_blocks = [
-#       "0.0.0.0/0"]
-#     description = ""
-#   }
-#   ingress {
-#     from_port = 80
-#     to_port = 80
-#     protocol = "tcp"
-#     ipv6_cidr_blocks = [
-#       "::/0"]
-#     description = "IPV6"
-#   }
-#   ingress {
-#     from_port = 80
-#     to_port = 80
-#     protocol = "tcp"
-#     self = true
-#     description = "Load Balancer HTTP"
-#   }
-#   ingress {
-#     from_port = 443
-#     to_port = 443
-#     protocol = "tcp"
-#     cidr_blocks = [
-#       "0.0.0.0/0"]
-#     description = ""
-#   }
-#   ingress {
-#     from_port = 443
-#     to_port = 443
-#     protocol = "tcp"
-#     ipv6_cidr_blocks = [
-#       "::/0"]
-#     description = ""
-#   }
-#   ingress {
-#     from_port = 443
-#     to_port = 443
-#     protocol = "tcp"
-#     self = true
-#     description = "Load Balance HTTP"
-#   }
-#   ingress {
-#     from_port = 9343
-#     to_port = 9343
-#     protocol = "tcp"
-#     self = true
-#     description = "Elasticsearch (transport client/transport client with TLS/SSL), also required by load balancers"
-#   }
-
-#   ingress {
-#     from_port = 9243
-#     to_port = 9243
-#     protocol = "tcp"
-#     self = true
-#     description = "Allow Elastic access only SSH"
-#   }
-
-#   egress {
-#     from_port = 9243
-#     to_port = 9243
-#     protocol = "tcp"
-#     cidr_blocks = [
-#       "${var.vpc_cidr}"]
-#     description = "Kibana and Elastic https traffic"
-#   }
-
-#   egress {
-#     from_port = 9200
-#     to_port = 9200
-#     protocol = "tcp"
-#     cidr_blocks = [
-#       "${var.vpc_cidr}"]
-#     description = "Kibana and Elastic http traffic"
-#   }
-
-#   egress {
-#     from_port = 443
-#     to_port = 443
-#     protocol = "tcp"
-#     cidr_blocks = [
-#       "0.0.0.0/0"]
-#     description = "Allow this port for Lambda functions to connect to Vault over https and AWS SES API"
-#   }
-#   tags = "${merge(
-#       var.common_tags,
-#       map(
-#         "Name", "sg.${var.org_scope}.${var.functional_scope}.${var.module_name}.esearch_kibana.${var.stage}"
-#       )
-#     )}"
-# }
--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -6,9 +6,6 @@ module "vpc" {
   azs                    = ["${data.aws_availability_zones.available.names[0]}", "${data.aws_availability_zones.available.names[1]}", "${data.aws_availability_zones.available.names[2]}"]
   public_subnets         = ["${cidrsubnet(module.vpc.vpc_cidr_block, 3, 0)}"]
   database_subnets       = ["${cidrsubnet(module.vpc.vpc_cidr_block, 3, 1)}", "${cidrsubnet(module.vpc.vpc_cidr_block, 3, 2)}", "${cidrsubnet(module.vpc.vpc_cidr_block, 3, 3)}"]
-  enable_nat_gateway     = true
-  single_nat_gateway     = true
-  one_nat_gateway_per_az = false
   enable_dns_hostnames   = true
   enable_dns_support     = true
   tags = {
